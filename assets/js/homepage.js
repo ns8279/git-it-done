266cae2cc4e89619965efc4bcfa44b267037da95
Nirupama Sundararaman <nirupama.2308@gmail.com>
2020-04-14 21:09:18 -0700

    finishing up disyplaying the data on the screen and error handaling

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,3 +1,8 @@
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+var repoContainerEl = document.querySelector("#repos-container");
+var repoSearchTerm = document.querySelector("#repo-search-term");
+
 
 // var response =  fetch("https://api.github.com/users/octocat/repos");
 // console.log(response);
@@ -8,12 +13,86 @@ var getUserRepos = function(user) {
 
     //make a request to the url
     fetch(apiUrl).then(function(response){
-        response.json().then(function(data){
-            console.log(data);
-        });
-    });
+        if (response.ok) {
+            response.json().then(function(data){
+                displayRepos(data, user);
+            });
+        }else {
+            alert("Error:" + response.statusText);
+        }   
+    })
+    .catch(function(error){
+        //Notice this '.catch()' getting chained onto the end of the .then() method
+        alert("unable to connect to GitHub! Please try again later");
+    })
 };
-  
 
+//function to be executed when the form submission browser event occurs
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    //console.log(event);
+//get value for input element
+    var username = nameInputEl.value.trim();
+
+    if (username) {
+        getUserRepos(username);
+        nameInputEl.value = "";
+    }else {
+        alert("Please enter a valid GiHub username");
+    }
+}
+
+//function to display the repository
+var displayRepos = function(repos, searchTerm) {
+    // console.log(repos);
+    // console.log(searchTerm);
+
+    //if there are no repositories
+    if(repos.length === 0){
+        repoContainerEl.textContent = "No repositories Found";
+        return;
+    }
+
+    //clear old data
+    repoContainerEl.textContent = "";
+    repoSearchTerm.textContent = searchTerm;
+
+    //loop over repos
+    for (var i = 0; i < repos.length; i++) {
+        // format repo name
+        var repoName = repos[i].owner.login + "/" + repos[i].name;
+      
+        // create a container for each repo
+        var repoEl = document.createElement("div");
+        repoEl.classList = "list-item flex-row justify-space-between align-center";
+      
+        // create a span element to hold repository name
+        var titleEl = document.createElement("span");
+        titleEl.textContent = repoName;
+
+        // create a status element
+        var statusEl = document.createElement("span");
+        statusEl.classList = "flex-row align-center";
+
+        // check if current repo has issues or not
+        if (repos[i].open_issues_count > 0) {
+            statusEl.innerHTML = "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
+        } else {
+            statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+        } 
+        // append to container
+        repoEl.appendChild(titleEl);
+
+        // append to container
+        repoEl.appendChild(statusEl);
+      
+        // append container to the dom
+        repoContainerEl.appendChild(repoEl);
+      }
+      
+
+}
+  
+userFormEl.addEventListener("submit", formSubmitHandler);
 getUserRepos();
 
